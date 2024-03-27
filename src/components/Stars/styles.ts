8dfd7c6a9ab28a4f5f35d70052ba32538b95f704
Marcos Renê <renemoura789@gmail.com>
2024-03-27 14:06:38 -0300

    feat: add stars component with animation

---
+++ src/components/Stars/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  canvas: {
+    position: 'absolute',
+    width: 257,
+    height: 249,
+    zIndex: 1,
+  }
+});
